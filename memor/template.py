2e5dac0f149d466ee44f1a633dd5d992777ebc86
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-04 18:36:28 +0330

    fix : CustomPromptTemplate updated

--- memor/template.py
+++ memor/template.py
@@ -1,19 +1,39 @@
 # -*- coding: utf-8 -*-
 """Template class."""
-from enum import Enum
 
 
 class CustomPromptTemplate:
     """Prompt template."""
 
-    def __init__(self, content):
+    def __init__(self, content, file_path=None):
         """
         Template object initiator.
 
         :param content: template content
         :type content: str
+        :param file_path: template file path
+        :type file_path: str
         """
         self._content = content
+
+    def save(self, file_path):
+        """
+        Save method.
+
+        :param file_path: template file path
+        :type file_path: str
+        :return: result as dict
+        """
+        pass
+
+    def to_json(self):
+        """Convert to json."""
+        pass
+
+    def to_dict(self):
+        "Convert to dict."
+        pass
+
 
 
 DEFAULT_TEMPLATE = CustomPromptTemplate(content="{message}")
