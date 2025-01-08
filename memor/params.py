f776ef49dc8fa090261133c9817fe038a942b00e
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-01-07 22:02:50 -0800

    add : error messages added.

--- memor/params.py
+++ memor/params.py
@@ -3,6 +3,9 @@
 from enum import Enum
 MEMOR_VERSION = "0.1"
 
+INVALID_PATH_MESSAGE = "Invalid path. Path must be a string."
+INVALID_NONSTR_VALUE_MESSAGE = "Invalid value. Value must be a string."
+INVALID_TEMPLATE_FILE_MESSAGE = "Invalid template file. It's either unreadable or not a JSON file with proper fields."
 DATA_SAVE_SUCCESS_MESSAGE = "Everything seems good."
 
 class PromptRenderFormat(Enum):
