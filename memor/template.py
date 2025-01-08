49bbde5551d2579697d5a8e6749b22762ee9f768
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-01-07 22:03:55 -0800

    update : template updated with validation.

--- memor/template.py
+++ memor/template.py
@@ -3,7 +3,11 @@
 import json
 import datetime
 from .params import DATA_SAVE_SUCCESS_MESSAGE
+from .params import INVALID_TEMPLATE_FILE_MESSAGE
 from .params import MEMOR_VERSION
+from .errors import MemorValidationError
+from .functions import validate_path
+from .functions import validate_template_content, validate_template_title
 
 
 class CustomPromptTemplate:
@@ -23,14 +27,11 @@ class CustomPromptTemplate:
         memor_version = MEMOR_VERSION
         date_created = str(datetime.datetime.now())
         if file_path:
-            with open(file_path, "r") as file:
-                loaded_obj = json.loads(file.read())
-                content = loaded_obj["content"]
-                title = loaded_obj["title"]
-                memor_version = loaded_obj["memor_version"]
-                date_created = loaded_obj["date_created"]
-        self._title = title
-        self._content = content
+            self.load(file_path)
+        if title:
+            self.update_title(title)
+        if content:
+            self.update_content(content)
         self._memor_version = memor_version
         self._date_created  = date_created
 
@@ -38,9 +39,11 @@ class CustomPromptTemplate:
         return self._content
 
     def update_title(self, title):
+        validate_template_title(title)
         self._title = title
 
     def update_content(self, content):
+        validate_template_content(content)
         self._content = content
 
     def save(self, file_path):
@@ -59,6 +62,25 @@ class CustomPromptTemplate:
             result["status"] = False
             result["message"] = str(e)
         return result
+
+    def load(self, file_path):
+        """
+        Load method.
+
+        :param file_path: template file path
+        :type file_path: str
+        :return: result as dict
+        """
+        validate_path(file_path)
+        try:
+            with open(file_path, "r") as file:
+                loaded_obj = json.loads(file.read())
+                self._content = loaded_obj["content"]
+                self._title = loaded_obj["title"]
+                self._memor_version = loaded_obj["memor_version"]
+                self._date_created = loaded_obj["date_created"]
+        except Exception:
+            raise MemorValidationError(INVALID_TEMPLATE_FILE_MESSAGE)
 
     def to_json(self):
         """Convert to json."""
