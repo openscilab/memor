f47cb49d13fe8bb51580861b41ef73f379b088b2
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-09 02:24:46 +0330

    fix : autopep8

--- memor/params.py
+++ memor/params.py
@@ -9,6 +9,7 @@ INVALID_NONSTR_VALUE_MESSAGE = "Invalid value. `{0}` must be a string.."
 INVALID_TEMPLATE_FILE_MESSAGE = "Invalid template file. It should be a JSON file with proper fields."
 DATA_SAVE_SUCCESS_MESSAGE = "Everything seems good."
 
+
 class PromptRenderFormat(Enum):
     """Prompt render format."""
 
