cd7046a6e7658487eafdcecf8a99b99924b5a5c4
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-01-08 22:19:54 -0800

    add : new error messages added.

--- memor/params.py
+++ memor/params.py
@@ -5,8 +5,12 @@ MEMOR_VERSION = "0.1"
 
 INVALID_PATH_MESSAGE = "Invalid path. Path must be a string."
 PATH_DOES_NOT_EXIST_MESSAGE = "Path does not exist."
-INVALID_NONSTR_VALUE_MESSAGE = "Invalid value. `{0}` must be a string.."
+INVALID_NONSTR_VALUE_MESSAGE = "Invalid value. `{0}` must be a string."
+INVALID_NONFLOAT_VALUE_MESSAGE = "Invalid value. `{0}` must be a float."
+INVALID_NONLIST_VALUE_MESSAGE = "Invalid value. `{0}` must be a list."
+INVALID_TEMPLATE_MESSAGE = "Invalid template. It must be an instance of CustomPromptTemplate object."
 INVALID_TEMPLATE_FILE_MESSAGE = "Invalid template file. It should be a JSON file with proper fields."
+INVALID_PROMPT_FILE_MESSAGE = "Invalid prompt file. It should be a JSON file with proper fields."
 DATA_SAVE_SUCCESS_MESSAGE = "Everything seems good."
 
 
