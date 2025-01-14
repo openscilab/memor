764867e4ca2b56cae0e9347d4d721aa97a96435e
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-01-13 17:24:37 -0800

    update : docstrings updated in functions.

--- memor/functions.py
+++ memor/functions.py
@@ -14,24 +14,9 @@ def get_time_utc():
     """
     Get time in UTC format.
 
-    :return: time in UTC format in datetime object
+    :return: UTC format time in datetime object
     """
     return datetime.datetime.now(datetime.timezone.utc)
-
-
-def validate_path(path):
-    """
-    Validate path.
-
-    :param path: path
-    :type path: Any
-    :return: True if path is valid
-    """
-    if not isinstance(path, str):
-        raise MemorValidationError(INVALID_PATH_MESSAGE)
-    if not os.path.exists(path):
-        raise FileNotFoundError(PATH_DOES_NOT_EXIST_MESSAGE.format(path))
-    return True
 
 
 def _validate_string(value, parameter_name):
@@ -42,7 +27,7 @@ def _validate_string(value, parameter_name):
     :type value: Any
     :param parameter_name: parameter name
     :type parameter_name: str
-    :return: True if value is valid
+    :return: True if value is a string
     """
     if not isinstance(value, str):
         raise MemorValidationError(INVALID_NONSTR_VALUE_MESSAGE.format(parameter_name))
@@ -51,13 +36,13 @@ def _validate_string(value, parameter_name):
 
 def _validate_pos_float(value, parameter_name):
     """
-    Validate float.
+    Validate positive float.
 
     :param value: value
     :type value: Any
     :param parameter_name: parameter name
     :type parameter_name: str
-    :return: True if value is valid
+    :return: True if value is a positive float
     """
     if not isinstance(value, float) or value < 0:
         raise MemorValidationError(INVALID_NON_POSFLOAT_VALUE_MESSAGE.format(parameter_name))
@@ -72,7 +57,7 @@ def _validate_list_of_str(value, parameter_name):
     :type value: Any
     :param parameter_name: parameter name
     :type parameter_name: str
-    :return: True if value is valid
+    :return: True if value is a list of strings
     """
     if not isinstance(value, list):
         raise MemorValidationError(INVALID_LIST_OF_STR_MESSAGE.format(parameter_name))
@@ -83,8 +68,24 @@ def _validate_list_of_str(value, parameter_name):
     return True
 
 
+def validate_path(path):
+    """
+    Validate path property.
+
+    :param path: path
+    :type path: Any
+    :return: True if path is a string and exists
+    """
+    if not isinstance(path, str):
+        raise MemorValidationError(INVALID_PATH_MESSAGE)
+    if not os.path.exists(path):
+        raise FileNotFoundError(PATH_DOES_NOT_EXIST_MESSAGE.format(path))
+    return True
+
+
 def validate_template_title(title):
-    """Validate title.
+    """
+    Validate title property in CustomPromptTemplate class.
 
     :param title: title
     :type title: Any
@@ -94,7 +95,8 @@ def validate_template_title(title):
 
 
 def validate_template_content(content):
-    """Validate template content.
+    """
+    Validate content property in CustomPromptTemplate class.
 
     :param content: content
     :type content: Any
@@ -103,51 +105,13 @@ def validate_template_content(content):
     return _validate_string(content, "content")
 
 
-def validate_prompt_message(message):
-    """Validate prompt message.
-
-    :param message: message
-    :type message: Any
-    :return: True if message is valid
-    """
-    return _validate_string(message, "message")
-
-
-def validate_prompt_responses(responses):
-    """Validate prompt responses.
-
-    :param responses: responses
-    :type responses: Any
-    :return: True if responses is valid
-    """
-    return _validate_list_of_str(responses, "responses")
-
-
-def validate_prompt_temperature(temperature):
-    """Validate prompt temperature.
-
-    :param temperature: temperature
-    :type temperature: Any
-    :return: True if temperature is valid
-    """
-    return _validate_pos_float(temperature, "temperature")
-
-
-def validate_prompt_model(model):
-    """Validate prompt model.
-
-    :param model: model
-    :type model: Any
-    :return: True if model is valid
-    """
-    return _validate_string(model, "model")
-
 def validate_custom_map(custom_map):
-    """Validate custom map.
+    """
+    Validate custom map property in CustomPromptTemplate class.
 
     :param custom_map: custom map
     :type custom_map: Any
-    :return: True if custom map is valid
+    :return: True if custom map is a dictionary with keys and values that can be converted to strings
     """
     if not isinstance(custom_map, dict):
         raise MemorValidationError(INVALID_COSUTOM_MAP_MESSAGE)
@@ -157,3 +121,47 @@ def validate_custom_map(custom_map):
     except Exception:
         raise MemorValidationError(INVALID_COSUTOM_MAP_MESSAGE)
     return True
+
+
+def validate_prompt_message(message):
+    """
+    Validate message property in Prompt class.
+
+    :param message: message
+    :type message: Any
+    :return: True if message is valid
+    """
+    return _validate_string(message, "message")
+
+
+def validate_prompt_responses(responses):
+    """
+    Validate responses property in Prompt class.
+
+    :param responses: responses
+    :type responses: Any
+    :return: True if responses is valid
+    """
+    return _validate_list_of_str(responses, "responses")
+
+
+def validate_prompt_temperature(temperature):
+    """
+    Validate temperature property in Prompt class.
+
+    :param temperature: temperature
+    :type temperature: Any
+    :return: True if temperature is valid
+    """
+    return _validate_pos_float(temperature, "temperature")
+
+
+def validate_prompt_model(model):
+    """
+    Validate model property in Prompt class.
+
+    :param model: model
+    :type model: Any
+    :return: True if model is valid
+    """
+    return _validate_string(model, "model")
