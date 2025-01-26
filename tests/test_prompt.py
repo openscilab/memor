7eff31bd77b53a5a622f48b330174fb76b18054f
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-26 07:37:32 +0330

    fix : minor bug in tests fixed

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -3,11 +3,15 @@ from memor import Prompt, Response, Role
 TEST_CASE_NAME = "Prompt tests"
 
 def test_prompt_message():
-    prompt = Prompt(message="Hello, how are you?", responses=["I am fine."], role=Role.USER)
+    message = "Hello, how are you?"
+    response = Response(message="I am fine.")
+    prompt = Prompt(message=message, responses=[response], role=Role.USER)
     assert prompt.message == "Hello, how are you?"
 
 def test_prompt_add_response():
-    prompt = Prompt(message="Hello, how are you?", responses=["I am fine."], role=Role.USER)
-    response = Response(message="Great!", role=Role.ASSISTANT)
-    prompt.add_response(response)
-    assert response in prompt.responses
+    message = "Hello, how are you?"
+    response0 = Response(message="I am fine.")
+    prompt = Prompt(message=message, responses=[response0], role=Role.USER)
+    response1 = Response(message="Great!", role=Role.ASSISTANT)
+    prompt.add_response(response1)
+    assert response1 in prompt.responses
