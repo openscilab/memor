e6e3a18cfbf05d080da21b08bf3ed25f9d709db2
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-30 16:52:48 +0330

    fix : test_response.py updated

--- tests/test_response.py
+++ tests/test_response.py
@@ -68,9 +68,17 @@ def test_response_date():
 
 def test_response_save():
     response = Response(message="I am fine.", model="GPT-4", temperature=0.5, role=Role.USER, score=0.8)
-    result = response.save("test.json")
-    with open("test.json", "r") as file:
+    result = response.save("response_test.json")
+    with open("response_test.json", "r") as file:
         saved_response = json.loads(file.read())
     assert result["status"] == True and response.to_dict() == saved_response
+
+
+def test_response_json():
+    response = Response(message="I am fine.", model="GPT-4", temperature=0.5, role=Role.USER, score=0.8)
+    assert response.to_json() == json.dumps(response.to_dict(), indent=4)
+
+
+
 
 
