5c5382e08155bbc4131af9ca0c8898f9e7f3bb5c
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-30 16:19:56 +0330

    fix : test_response.py updated

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,3 +1,4 @@
+import datetime
 from memor import Response, Role
 
 TEST_CASE_NAME = "Response tests"
@@ -11,3 +12,26 @@ def test_response_message():
 def test_response_score():
     response = Response(message="I am fine.", score=0.9, role=Role.ASSISTANT)
     assert response.score == 0.9
+
+
+def test_response_role():
+    response = Response(message="I am fine.", score=0.9, role=Role.ASSISTANT)
+    assert response.role == Role.ASSISTANT
+
+
+def test_response_temperature():
+    response = Response(message="I am fine.", temperature=0.2)
+    assert response.temperature == 0.2
+
+
+def test_response_model():
+    response = Response(message="I am fine.", model="GPT-4")
+    assert response.model == "GPT-4"
+
+
+def test_response_date():
+    date_time_utc = datetime.datetime.now(datetime.timezone.utc)
+    response = Response(message="I am fine.", date=date_time_utc)
+    assert response.date_created == date_time_utc
+
+
