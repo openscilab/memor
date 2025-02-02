40e79c1c9204f13efa7e122a85f2daa115d4d0df
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-02-03 01:13:22 +0330

    fix : test_custom_prompt_template.py updated

--- tests/test_custom_prompt_template.py
+++ tests/test_custom_prompt_template.py
@@ -3,6 +3,23 @@ from memor import CustomPromptTemplate
 TEST_CASE_NAME = "CustomPromptTemplate tests"
 
 
+def test_equality1():
+    template1 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"})
+    template2 = template1.copy()
+    assert template1 == template2
+
+
+def test_equality2():
+    template1 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template1")
+    template2 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template2")
+    assert template1 != template2
+
+
+def test_equality3():
+    template1 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template1")
+    template2 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template1")
+    assert template1 == template2
+
 def test_custom_prompt_template_content():
     template = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"})
     assert template.content == "What is your name?"
