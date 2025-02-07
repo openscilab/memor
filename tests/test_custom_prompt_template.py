4191c4a483e71a3967f342d156223865e8059625
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-02-07 19:47:22 +0330

    fix : test_custom_prompt_template.py updated

--- tests/test_custom_prompt_template.py
+++ tests/test_custom_prompt_template.py
@@ -4,28 +4,28 @@ TEST_CASE_NAME = "CustomPromptTemplate tests"
 
 
 def test_equality1():
-    template1 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"})
+    template1 = CustomPromptTemplate(content="Act as a {language} developer and respond to this question:\n{prompt_message}", custom_map={"language": "Python"})
     template2 = template1.copy()
     assert template1 == template2
 
 
 def test_equality2():
-    template1 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template1")
-    template2 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template2")
+    template1 = CustomPromptTemplate(content="Act as a {language} developer and respond to this question:\n{prompt_message}", custom_map={"language": "Python"}, title="template1")
+    template2 = CustomPromptTemplate(content="Act as a {language} developer and respond to this question:\n{prompt_message}", custom_map={"language": "Python"}, title="template2")
     assert template1 != template2
 
 
 def test_equality3():
-    template1 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template1")
-    template2 = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"}, title="template1")
+    template1 = CustomPromptTemplate(content="Act as a {language} developer and respond to this question:\n{prompt_message}", custom_map={"language": "Python"}, title="template1")
+    template2 = CustomPromptTemplate(content="Act as a {language} developer and respond to this question:\n{prompt_message}", custom_map={"language": "Python"}, title="template1")
     assert template1 == template2
 
 
-def test_custom_prompt_template_content():
-    template = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"})
-    assert template.content == "What is your name?"
+def test_content():
+    template = CustomPromptTemplate(content="Act as a {language} developer and respond to this question:\n{prompt_message}", custom_map={"language": "Python"})
+    assert template.content == "Act as a {language} developer and respond to this question:\n{prompt_message}"
 
 
-def test_custom_prompt_template_custom_map():
-    template = CustomPromptTemplate(content="What is your name?", custom_map={"name": "John"})
-    assert template.custom_map == {"name": "John"}
+def test_custom_map():
+    template = CustomPromptTemplate(content="Act as a {language} developer and respond to this question:\n{prompt_message}", custom_map={"language": "Python"})
+    assert template.custom_map == {"language": "Python"}
