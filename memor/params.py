5c2202dd5bc700bcb7d7470c573b0bbc28783d89
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-07-14 11:40:08 +0330

    fix : GPT4O search models added

--- memor/params.py
+++ memor/params.py
@@ -62,6 +62,8 @@ class LLMModel(Enum):
     GPT_4_VISION = "gpt-4-vision"
     GPT_4O = "gpt-4o"
     GPT_4O_MINI = "gpt-4o-mini"
+    GPT_4O_SEARCH = "gpt-4o-search"
+    GPT_4O_MINI_SEARCH = "gpt-4o-mini-search"
     GPT_4_1 = "gpt-4.1"
     GPT_4_1_MINI = "gpt-4.1-mini"
     GPT_4_1_NANO = "gpt-4.1-nano"
