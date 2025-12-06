9120466672758823539d2b571954ff0b8931d76d
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-12-06 13:57:24 +0330

    fix : codacy issues fixed

--- memor/session.py
+++ memor/session.py
@@ -580,7 +580,7 @@ class Session:
         self._warnings["size"]["enable"] = False
         self._warnings["size"]["threshold"] = 0
         self._mark_modified()
-    
+
     def reset_render_counter(self) -> None:
         """Reset the render counter."""
         self._render_counter = 0
