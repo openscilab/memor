dfe16259f52f03958d748d4f057beaea8ce84f68
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-20 12:05:39 +0330

    fix : update tests

--- tests/test_session.py
+++ tests/test_session.py
@@ -142,8 +142,10 @@ def test_enable_message():
     response = Response(message="I am fine.")
     session = Session(messages=[prompt, response])
     session.update_messages_status([False, False])
+    session_date_modified = session.date_modified
     session.enable_message(0)
     assert session.messages_status == [True, False]
+    assert session.date_modified != session_date_modified
 
 
 def test_disable_message():
@@ -151,8 +153,10 @@ def test_disable_message():
     response = Response(message="I am fine.")
     session = Session(messages=[prompt, response])
     session.update_messages_status([True, True])
+    session_date_modified = session.date_modified
     session.disable_message(0)
     assert session.messages_status == [False, True]
+    assert session.date_modified != session_date_modified
 
 
 def test_mask_message():
