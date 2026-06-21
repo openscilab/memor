484773b9ab196ef6e0e288f3b852526a4c5bd73f
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-06-21 08:17:03 +0330

    Mark Modification Bug (#228)
    * fix : update enable_message/disable_message methods
    
    * fix : update tests
    
    * doc : update CHANGELOG.md
    
    * fix : update tests
    
    * fix : update tests
    
    * fix : update tests
    
    * doc : update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 ### Changed
+- `Session` class `enable_message` method modified
+- `Session` class `disable_message` method modified
+- `Session` class `mask_message` method modified
+- `Session` class `unmask_message` method modified
 - Test system modified
 - `Response` class `date` bug fixed
 ## [1.2] - 2026-06-15
--- tests/test_session.py
+++ tests/test_session.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 import datetime
 import copy
 import pytest
@@ -142,8 +143,11 @@ def test_enable_message():
     response = Response(message="I am fine.")
     session = Session(messages=[prompt, response])
     session.update_messages_status([False, False])
+    session_date_modified = session.date_modified
+    time.sleep(2)
     session.enable_message(0)
     assert session.messages_status == [True, False]
+    assert session.date_modified > session_date_modified
 
 
 def test_disable_message():
@@ -151,16 +155,22 @@ def test_disable_message():
     response = Response(message="I am fine.")
     session = Session(messages=[prompt, response])
     session.update_messages_status([True, True])
+    session_date_modified = session.date_modified
+    time.sleep(2)
     session.disable_message(0)
     assert session.messages_status == [False, True]
+    assert session.date_modified > session_date_modified
 
 
 def test_mask_message():
     prompt = Prompt(message="Hello, how are you?", role=Role.USER)
     response = Response(message="I am fine.")
     session = Session(messages=[prompt, response])
+    session_date_modified = session.date_modified
+    time.sleep(2)
     session.mask_message(0)
     assert session.messages_status == [False, True]
+    assert session.date_modified > session_date_modified
 
 
 def test_unmask_message():
@@ -168,8 +178,11 @@ def test_unmask_message():
     response = Response(message="I am fine.")
     session = Session(messages=[prompt, response])
     session.update_messages_status([False, False])
+    session_date_modified = session.date_modified
+    time.sleep(2)
     session.unmask_message(0)
     assert session.messages_status == [True, False]
+    assert session.date_modified > session_date_modified
 
 
 def test_masks():
